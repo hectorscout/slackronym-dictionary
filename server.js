10f5086ed9742b3262788f6c5c7764ed8835cd76
Tyson Holmes <tyson.holmes@rms.com>
2018-07-31 13:01:25 +0200

    use config variables

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-const dbConfig             = require('./config/db');
+// const dbConfig       = require('./config/db');
 
 const app            = express();
 
-const port = 8888;
+const port = process.env.SLACRONYM_PORT or 8888;
+const dbUrl = process.env.DB_URL;
     
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
 
-
-// MongoClient.connect(db.url, (err, database) => {
-//     if (err) return console.log(err)
-    
-//     db = database.db("slackronym-dictionary")
-//     require('./app/routes')(app, db);
-    
-//     app.listen(port, () => {
-// 	console.log('We are live on ' + port);
-//     });
-// });    
-
-MongoClient.connect(dbConfig.url, (err, database) => {
+MongoClient.connect(dbUrl, (err, database) => {
   if (err) return console.log(err)
                       
   // Make sure you add the database name and not the collection name
