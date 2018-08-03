d890aaf1f1b7d67c672742da9329d415d8e17b5c
Tyson Holmes <tyson.holmes@rms.com>
2018-08-03 19:03:17 +0200

    a pretty functional version

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,23 +1,57 @@
-module.exports = function(app, db) {
+module.exports = function(app, db, web) {
+    const token = process.env.SLACKRONYM_TOKEN;
     const request = require('request');
 
     const ADD_ACK_ID = 'addAck';
     const DEFINE_ACK_DIALOG_ID = 'defineAckDialog';
+    const DUMB_VALUE = 'A very dumb thing that noone will type into the thing';
     
-    const token = process.env.SLACKRONYM_TOKEN;
+    let messageIds = {};
 
-    _getAvailableAcronyms = () => {
-	return ['tacos', 'stuff']
-    }
-
-
-    _getListResponse = () => {
-        acronyms = _getAvailableAcronyms();
-        acronyms.sort()
+ 
+    _getItemMessage = (item, update) => {
+	itemAttachment = {
+	    title: `${item.acronym}:`,
+	    text: item.definition,
+	    fields: []
+	}
+	if(item.docUrl) {
+	    itemAttachment.fields.push({
+		title: 'Documentation',
+		value: item.docUrl,
+		short: false
+	    });
+	}
+	if (update) {
+	    itemAttachment.callback_id = ADD_ACK_ID;
+	    itemAttachment.actions = [{
+		name: 'request',
+		text: 'Update',
+		type: 'button',
+		value: key
+	    }];
+	}
+	
 	return {
-	    text: 'Available Acronyms:',
-	    attachments: [{text: acronyms.join(', ')}]
+	    text: '',
+	    attachments: [itemAttachment]
 	};
+    }
+    
+
+    _getListResponse = (callback) => {
+	db.collection('definitions').distinct('acronym', {}), (err, acronyms) => {
+	    if (err) {
+		callback({'There was an error trying to get all the available acronyms. Please try again later.'});
+	    }
+	    else {
+		acronyms.sort()
+		callback({
+		    text: 'Available Acronyms:',
+		    attachments: [{text: acronyms.join(', ')}]
+		});
+	    }
+	});
     }
 
 
@@ -36,7 +70,7 @@ module.exports = function(app, db) {
 		    name: 'dumb',
 		    text: 'I was just being dumb.',
 		    type: 'button',
-		    value: 'dumb'
+		    value: DUMB_VALUE
 		}]
 	    }]
 	};
@@ -47,9 +81,7 @@ module.exports = function(app, db) {
 	const cursor = db.collection('definitions').find({acronym: acronym}).sort({timestamp: -1}).limit(1);
 	cursor.count((err, count) => {
 	    if (err) callback(err);
-	    console.log('gettttttttttttttttttttttttttttt', count);
 	    if (!count) {
-		console.log('returing cuz no count');
 		return callback(null, {acronym: acronym});
 	    }
 	});
@@ -62,57 +94,45 @@ module.exports = function(app, db) {
     
 
     _openAddDialog = (triggerId, options) => {
-	console.log({options: options});
 	_getDefinitionsItem(options.acronym, (err, item) => {
-	    console.log('item:', item);
 	    if (err) {
-		console.log('There was an error trying to get a definition:', {error: err});
+		return console.log('There was an error trying to get a definition:', {error: err});
 	    }
-	    options = {
-		url: 'https://slack.com/api/dialog.open',
-		headers: {
-		    Authorization: `Bearer ${token}`,
-		    'Content-type': 'application/json'
-		},
-		body: JSON.stringify({
-		    trigger_id: triggerId,
-		    dialog: {
-			callback_id: DEFINE_ACK_DIALOG_ID,
-			title: 'AAA (Add An Acronym)',
-			submit_label: 'Add',
-			elements: [
-			    {
-				type: 'text',
-				label: 'Acronym',
-				name: 'acronym',
-				value: options.acronym
-			    },
-			    {
-				type: 'text',
-				label: 'Definition',
-				name: 'definition',
-				optional: true,
-				hint: "If you don't know, just leave it blank and we'll try to figure it out.",
-				value: item.definition
-			    },
-			    {
-				type: 'text',
-				label: 'Documentation Link',
-				name: 'docUrl',
-				optional: true,
-				subType: 'url',
-				value: item.docUrl
-			    }
-			]
-		    }
-		})
-	    }
-	    request.post(options, (err, response, body) => {
-		console.log('eeeeeeeerrrrrrrrrrooooooooorrrrrrrrrr', err);
-		console.log(body);
+	    web.dialog.open({
+		trigger_id: triggerId,
+		dialog: {
+		    callback_id: DEFINE_ACK_DIALOG_ID,
+		    title: 'AAA (Add An Acronym)',
+		    submit_label: 'Add',
+		    notify_on_cancel: true,
+		    elements: [
+			{
+			    type: 'text',
+			    label: 'Acronym',
+			    name: 'acronym',
+			    value: options.acronym
+			},
+			{
+			    type: 'text',
+			    label: 'Definition',
+			    name: 'definition',
+			    optional: true,
+			    hint: "If you don't know, just leave it blank and we'll try to figure it out.",
+			    value: item.definition
+			},
+			{
+			    type: 'text',
+			    label: 'Documentation Link',
+			    name: 'docUrl',
+			    optional: true,
+			    subType: 'url',
+			    value: item.docUrl,
+			    hint: "FYI: If this is dumb, we'll just remove it... (and ban you ¯\\_(ツ)_/¯)"
+			}
+		    ]
+		}
 	    });
 	});
-	
     }
 
 
@@ -128,41 +148,73 @@ module.exports = function(app, db) {
     }
 
 
-    _updateCallback = (err, item, res) => {
-	console.log('in callback');
+    _updateMessage = (userId, message) => {
+	options = {
+	    url: messageIds[userId],
+	    headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-type': 'application/json'
+	    },
+	    body: JSON.stringify(message)
+	};
+	request.post(options, (err, response, body) => {
+	    console.log('err', err);
+	    console.log('response', response);
+	    console.log('body', body);
+	});
+    }
+    
+
+    _updateCallback = (err, result, res, userId) => {
 	if (err) {
 	    res.send('There was... a _problem_...');
 	}
 	else {
-	    console.log('successfully created:', item);
-	    res.send({text: `We successfully updated the dictionary with your definition of \`${item.acronym}\`.`});
+	    item = result.ops[0];
+	    _updateMessage(userId, {text: `We successfully updated the dictionary with your definition of \`${item.acronym}\`.`});
+	    res.send();
 	}
     }
     
-    
+    // Commands from interactive stuff like buttons and dialogs
     app.post('/request', (req, res) => {
 	payload = JSON.parse(req.body.payload);
+	console.log('/request');
 	console.log('payload', payload);
+	
 	switch (payload.callback_id) {
 	case ADD_ACK_ID:
-	    action = payload.actions[0];
-	    _openAddDialog(payload.trigger_id, {acronym: action.value});
-	    res.send();
+            // Clicked a `We should add it`, `I was just being dumb`, or `Update` button
+	    console.log(payload.actions[0]);
+	    value = payload.actions[0].value;
+	    if (value === DUMB_VALUE) return res.send({text: 'https://media1.giphy.com/media/l0ExsczAepXFxWSw8/200.gif'});
+	    messageIds[payload.user.id] = payload.response_url;
+	    _openAddDialog(payload.trigger_id, {acronym: value});
+	    res.send({text: 'Defining an acronym (_like a boss_)'});
 	    break;
 	case DEFINE_ACK_DIALOG_ID:
-	    _updateDefinition({
-		acronym: payload.submission.acronym,
-		definition: payload.submission.definition,
-		docUrl: payload.submission.docUrl,
-		username: payload.user.name
-	    }, (err, item) => _updateCallback(err, item, res));
-	    
+	    // Closed the add definitions dialog
+	    if (payload.type === 'dialog_cancellation') {
+		_updateMessage(payload.user.id, {text: 'You did the right thing.'});
+	    }
+	    else {
+		_updateDefinition({
+		    acronym: payload.submission.acronym,
+		    definition: payload.submission.definition,
+		    docUrl: payload.submission.docUrl,
+		    username: payload.user.name
+		}, (err, result) => _updateCallback(err, result, res, payload.user.id));
+	    }
+	    res.send();
 	    break;
 	}
     });
 
+    // Main commands
     app.post('/lookup', (req, res) => {
-	console.log('doing a look up');
+	console.log('/lookup');
+	console.log('request body', req.body);
+
 	key = req.body.text.toUpperCase()
 	const update = key.split(' ')[0] === 'UPDATE'
 	if (update) {
@@ -170,51 +222,25 @@ module.exports = function(app, db) {
 	}
 
 	if (['LIST', 'HELP'].includes(key)) {
-	    res.send(_getListResponse());
+	    return _getListResponse((response) => res.send(response));
+	    // return res.send(_getListResponse());
 	}
-	else {
-	    
-	    const cursor = db.collection('definitions').find({acronym: key}).sort({timestamp: -1}).limit(1)
-	    console.log('here');
-	    // console.log('count', cursor.count());
-	    cursor.count((err, count) => {
+
+	_getDefinitionsItem(key, (err, item) => {
+	    if (err) {
 		console.log('err:', err);
-		console.log('count:', count);
-		if (!count) {
-		    res.send(_getUnknownResponse(req.body.text));		    
-		}
-	    });
-	    cursor.each((err, item) => {
-		console.log(item);
-		if (err) {
-		    res.send({error: err});
-		}
-		else if (item){
-		    console.log(item);
-		    attachments = [{text: item.definition}]
-		    if (update) {
-			attachments.push({
-			    callback_id: ADD_ACK_ID,
-			    actions:
-			    [{
-				name: 'request',
-				text: 'Update',
-				type: 'button',
-				value: key
-			    }]
-			});
-		    }
-		    res.send({
-			text: `${item.acronym}:`,
-			attachments: attachments
-		    });
-		}
-	    });
-	// });
-	}
+		res.send({error: err});
+	    }
+	    else if (!item.definition) {
+		res.send(_getUnknownResponse(req.body.text));
+	    }
+	    else {
+		res.send(_getItemMessage(item, update));
+	    }
+	});
     });
 
-
+    // Not using this
     app.post('/add', (req, res) => {
     	definition = {
     	    acronym: req.body.acronym.toUpperCase(),
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-
-const app            = express();
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const { WebClient } = require('@slack/client');
 
 const port = process.env.SLACKRONYM_PORT || 8080;
 const dbUrl = process.env.SLACKRONYM_DB_URL;
 const dbName = process.env.SLACKRONYM_DB_NAME;
+const token = process.env.SLACKRONYM_TOKEN;
+
+const app = express();
+const web = new WebClient(token);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,7 +18,7 @@ MongoClient.connect(dbUrl, (err, database) => {
                       
     db = database.db(dbName);
 
-    require('./app/routes')(app, db);
+    require('./app/routes')(app, db, web);
 
     app.listen(port, () => {
 	console.log('We are live on ' + port);
